8040bef1965eb82c4bf7f06d4d49d5f17bc56743
my-temporary-name <ankityadav123774@gmail.com>
2024-04-15 14:03:11 +0530

    data ingestion notebook added

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from textsummarizer.logging import logger
+from textsummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("welcome to our custom log")
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>> Stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>> Stage {STAGE_NAME} completed successfully <<<<<< \n\n\n x=================x")  
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
