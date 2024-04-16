c3a81bba5cbf98510a75d8b095443c9ac1083e19
my-temporary-name <ankityadav123774@gmail.com>
2024-04-16 21:12:13 +0530

    Data-transformation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from textsummarizer.logging import logger
 from textsummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textsummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from textsummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -21,6 +22,18 @@ try:
     logger.info(f">>>>> Stage {STAGE_NAME} started <<<<<<")
     data_validation = DataValidationTrainingPipeline()
     data_validation.main()
+    logger.info(f">>>>> Stage {STAGE_NAME} completed successfully <<<<<< \n\n\n x=================x")  
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation stage"
+
+try:
+    logger.info(f">>>>> Stage {STAGE_NAME} started <<<<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
     logger.info(f">>>>> Stage {STAGE_NAME} completed successfully <<<<<< \n\n\n x=================x")  
 
 except Exception as e:
